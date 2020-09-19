743d3f8ce878f0b40065477aa18481088b2593c2
Antonio Hernandez <antonio.hernandez5815@alumnos.udg.mx>
2020-09-19 10:19:47 -0500

    Añadimos nuevas variables y pusimos un caso 1 en el switch

--- programa.c
+++ programa.c
@@ -15,7 +15,29 @@ void mostrar(int n,char const *arreglo[])
 int main()
 {
 	int num;
+	int numeros[5];
+	int i,total=0;
+	float promedio=0;
 	printf("Ingresa el procedimiento a realizar:\n1.- Arreglo de 5 numeros\n2.- Funcion Mostrar\n3.- Estructura personaje\n");
 	scanf("%i",&num);
+	switch(num)
+	{
+		case 1:
+		for(i=0;i<5;i++)
+		{
+		printf("Ingresa el numero [%i]: ",i+1);
+		scanf("%i",&numeros[i]);
+		}
+		printf("\nTus numeros son:\n ");
+		for(i=0;i<5;i++)
+		{
+		printf("%i ",numeros[i]);
+		total+=numeros[i];
+		promedio = total/5;
+		}
+		printf("\nLa suma es: %i",total);
+		printf("\nEl promedio es: %.2f\n",promedio);
+		break;
+	}
 }
 	
